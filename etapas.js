bbd09fff97fe4666f48d7a64e9159076a237da38
Andre Mauri <79666075+andremauri@users.noreply.github.com>
2021-10-19 09:39:13 -0400

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Flanders',
+                partido: 'Back-End Rules',
+                fotos: [
+                    { url: 'flanders.jpg', legenda: 'Vereador', small: false }
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Burns',
+                partido: 'Full-Stack Mans',
+                fotos: [
+                    { url: 'burns.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '15',
+                nome: 'Homer Simpson',
+                partido: 'Front-End Rocks',
+                vice: 'Lisa Simpson',
+                fotos: [
+                    { url: 'homer.jpg', legenda: 'Prefeito' },
+                    { url: 'lisa.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '54',
+                nome: 'Quimby Mayor',
+                partido: 'Mobile First',
+                vice: 'Smithers',
+                fotos: [
+                    { url: 'quimby.jpg', legenda: 'Prefeito' },
+                    { url: 'smithers.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
